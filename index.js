bf1bde8ea8b3798aed351575e720ded62caf2039
David McGrann <dpmcgrann@gmail.com>
2019-02-02 20:09:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function searchRepositories() {
   const searchTerms = ${"#searchTerms"}.val();
-  const url = "https://api.github.com//search/repositories?q=${searchTerms}"
+  const url = "https://api.github.com//search/repositories?q=${searchTerms}";
+  $get(data)
 }
 
 
