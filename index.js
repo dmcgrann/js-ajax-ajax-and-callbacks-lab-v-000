dcf2bf498bdc8fcb15a144a72db5b0a7b7fee5ed
David McGrann <dpmcgrann@gmail.com>
2019-02-02 19:49:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-$(document).ready(function (){
+$(document).ready(function (searchRepositories){
+  var searchTerms =
+    'https://api.github.com/repos/rails/rails/commits?sha=82885325e04d78fb7ec608a4670164d842d23078';
+ 
+  $.get(searchTerms).done(function(results) {
+      return results;
+  });
 });
