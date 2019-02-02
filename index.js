4010a33be5a276620bb9c029afdf20b74e6110ba
David McGrann <dpmcgrann@gmail.com>
2019-02-02 20:21:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,19 @@ function displayError() {
   $("errors").html("I'm sorry, there's been an error. Please try again.");
 }
 
+function showCommits() {
+  const detailString = details.map(detail => {
+    return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      `
+  }
+    )
+}
+
 
 $(document).ready(function (){
 });
