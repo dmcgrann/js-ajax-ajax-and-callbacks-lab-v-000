7918248bba086c9ecff7978de5ff410087763273
David McGrann <dpmcgrann@gmail.com>
2019-02-02 19:57:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-$(document).ready(function (searchRepositories){
-  var searchTerms =
-    'https://api.github.com/repos/rails/rails/commits?sha=82885325e04d78fb7ec608a4670164d842d23078';
- 
-  $.get(searchTerms).done(function(results) {
-    return results
-});
+const base = 
+
+
+
+
+$(document).ready(function (){
 });
