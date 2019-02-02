4740471b2ad2083a0f80f3dd7bb053421997c78b
David McGrann <dpmcgrann@gmail.com>
2019-02-02 20:25:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,19 +21,10 @@ function displayError() {
   $("errors").html("I'm sorry, there's been an error. Please try again.");
 }
 
-function showCommits() {
+function showCommits(el) {
   const commits = JSON.parse(this.responseText);
   const commitsList = `<ul>${commits
-    .map(
-      commit =>
-        '<li><strong>' +
-        commit.author.login +
-        '</strong> - ' +
-        commit.commit.message +
-        '</li>'
-    )
-    .join('')}</ul>`;
-  document.getElementById('details').innerHTML = commitsList;
+  
 }
 
 
